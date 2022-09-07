16cc72401c74e12ca8f1a2669f84f6a08a43c24e
Anshul Kumar <anshul.kumar04@sap.com>
2022-09-07 13:13:29 +0530

    Updating Google Map Link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -168,7 +168,8 @@ export class AppComponent {
     var latlongvalue = position.coords.latitude + "," + position.coords.longitude;
     var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlongvalue + "&amp;zoom=14&amp;size=400x300&amp;key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
     let map = document.getElementById("mapholder");
-    let googleMapDirectionUrl = "https://www.google.com/maps/dir/29.868071,77.894844/29.865191,77.893944/@29.8661524,77.8933685,17.12z/data=!4m5!4m4!1m0!1m1!4e1!3e2";
+    let googleMapDirectionUrl = "https://www.google.com/maps/search/?api=1&query=" + latlongvalue;
+    // let googleMapDirectionUrl = "https://www.google.com/maps/dir/29.868071,77.894844/29.865191,77.893944/@29.8661524,77.8933685,17.12z/data=!4m5!4m4!1m0!1m1!4e1!3e2";
     if (map) {
       map.innerHTML =
         `<p><b>Current Location: ${latlongvalue} </b><button onclick="window.open('${googleMapDirectionUrl}','_blank')">Get Directions</button></p>
